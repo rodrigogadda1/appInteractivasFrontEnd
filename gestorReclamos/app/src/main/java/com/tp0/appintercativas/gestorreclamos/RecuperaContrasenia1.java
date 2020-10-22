1e086423d6ac9f76a8c7017562dc1575052e49b0
Rodrigo Gadda <u905693@proveedor.teco.com.ar>
2020-10-22 00:08:40 -0300

    agregado de pantallas

---
+++ gestorReclamos/app/src/main/java/com/tp0/appintercativas/gestorreclamos/RecuperaContrasenia1.java
@@ -0,0 +1,14 @@
+package com.tp0.appintercativas.gestorreclamos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecuperaContrasenia1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recupera_contrasenia1);
+    }
+}
