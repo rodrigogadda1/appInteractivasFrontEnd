1f8d3bcc1ab2a5f0287de646839a6ffa22aeec8a
Rodrigo Gadda <u905693@proveedor.teco.com.ar>
2020-11-02 00:55:32 -0300

    agregado de pantalla principal

--- gestorReclamos/app/src/main/java/com/tp0/appintercativas/gestorreclamos/PantallaPrincipal.java
+++ gestorReclamos/app/src/main/java/com/tp0/appintercativas/gestorreclamos/PantallaPrincipal.java
@@ -2,6 +2,9 @@ package com.tp0.appintercativas.gestorreclamos;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,18 +13,47 @@ import com.tp0.appintercativas.gestorreclamos.UserManagement.data.User;
 
 public class PantallaPrincipal extends AppCompatActivity {
 
-    private TextView txtDatos;
+    private ScrollView ScrollViewReclamos;
+    private Button btnNotificaciones, btnHistorialReclamos, btnReclamosActivos, btnReclamoNuevo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_principal);
 
-        txtDatos = (TextView) findViewById(R.id.txtDatos);
+        ScrollViewReclamos = (ScrollView) findViewById(R.id.ScrollViewReclamos);
+
+        btnNotificaciones = (Button) findViewById(R.id.btnNotificaciones);
+        btnHistorialReclamos = (Button) findViewById(R.id.btnHistorialReclamos);
+        btnReclamosActivos = (Button) findViewById(R.id.btnReclamosActivos);
+        btnReclamoNuevo = (Button) findViewById(R.id.btnReclamoNuevo);
+
+        btnNotificaciones.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //aca va que hace
+            }
+        });
+
+        btnHistorialReclamos.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //aca va que hace
+            }
+        });
+
+        btnReclamosActivos.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //aca va que hace
+            }
+        });
+
+        btnReclamoNuevo.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //aca va que hace
+            }
+        });
 
         Intent intent = getIntent();
         User user = (User) intent.getSerializableExtra("user");
 
-        txtDatos.setText(user.toString());
     }
 }
