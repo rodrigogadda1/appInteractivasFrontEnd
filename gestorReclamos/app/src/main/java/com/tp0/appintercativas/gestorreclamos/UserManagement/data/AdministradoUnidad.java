7cf77b0546e12b58330ffcd4f13a077bd7e49898
Rodrigo Gadda <u905693@proveedor.teco.com.ar>
2020-11-02 00:00:16 -0300

    no se porque hay que agregar esto para que abra

---
+++ gestorReclamos/app/src/main/java/com/tp0/appintercativas/gestorreclamos/UserManagement/data/AdministradoUnidad.java
@@ -0,0 +1,62 @@
+package com.tp0.appintercativas.gestorreclamos.UserManagement.data;
+
+public class AdministradoUnidad {
+
+    private long id_administradounidad;
+    private Administrado administrado;
+    private Unidad unidad;
+    private String relacion;
+
+    public AdministradoUnidad(long id_administradounidad, Administrado administrado, Unidad unidad, String relacion) {
+        this.id_administradounidad = id_administradounidad;
+        this.administrado = administrado;
+        this.unidad = unidad;
+        this.relacion = relacion;
+    }
+
+    public AdministradoUnidad() {
+        super();
+    }
+
+    @Override
+    public String toString() {
+        return "AdministradoUnidad{" +
+                "id_administradounidad=" + id_administradounidad +
+                ", administrado=" + administrado +
+                ", unidad=" + unidad +
+                ", relacion='" + relacion + '\'' +
+                '}';
+    }
+
+    public long getId_administradounidad() {
+        return id_administradounidad;
+    }
+
+    public void setId_administradounidad(long id_administradounidad) {
+        this.id_administradounidad = id_administradounidad;
+    }
+
+    public Administrado getAdministrado() {
+        return administrado;
+    }
+
+    public void setAdministrado(Administrado administrado) {
+        this.administrado = administrado;
+    }
+
+    public Unidad getUnidad() {
+        return unidad;
+    }
+
+    public void setUnidad(Unidad unidad) {
+        this.unidad = unidad;
+    }
+
+    public String getRelacion() {
+        return relacion;
+    }
+
+    public void setRelacion(String relacion) {
+        this.relacion = relacion;
+    }
+}
---
+++ gestorReclamos/app/src/main/java/com/tp0/appintercativas/gestorreclamos/UserManagement/data/EspacioComun.java
@@ -0,0 +1,62 @@
+package com.tp0.appintercativas.gestorreclamos.UserManagement.data;
+
+public class EspacioComun {
+
+    private long id_espaciocomun;
+    private String nombre;
+    private String descripcion;
+    private Edificio edificio;
+
+    public EspacioComun(long id_espaciocomun, String nombre, String descripcion, Edificio edificio) {
+        this.id_espaciocomun = id_espaciocomun;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.edificio = edificio;
+    }
+
+    public EspacioComun (){
+        super();
+    }
+
+    @Override
+    public String toString() {
+        return "EspacioComun{" +
+                "id_espaciocomun=" + id_espaciocomun +
+                ", nombre='" + nombre + '\'' +
+                ", descripcion='" + descripcion + '\'' +
+                ", edificio=" + edificio.toString() +
+                '}';
+    }
+
+    public long getId_espaciocomun() {
+        return id_espaciocomun;
+    }
+
+    public void setId_espaciocomun(long id_espaciocomun) {
+        this.id_espaciocomun = id_espaciocomun;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Edificio getEdificio() {
+        return edificio;
+    }
+
+    public void setEdificio(Edificio edificio) {
+        this.edificio = edificio;
+    }
+}
